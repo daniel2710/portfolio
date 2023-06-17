ea6981e67e08fe0a7da4b40d0df1f735642318e7
daniel2710 <daniel2710es@gmail.com>
2023-06-16 21:18:35 -0500

    skills section

--- views/Skills.tsx
+++ views/Skills.tsx
@@ -1,36 +1,28 @@
 import Image from 'next/image';
 import Line from '../public/img/line_pink.svg';
-import { projects } from '@/data/projects';
+import { skills } from '@/data/skills';
 
 const Skills = () => {
   return (
     <section className='pt-[34px] px-4 bg-main'>
         <div className='flex gap-2'>
-            <h2 className='text-white text-2xl'><span className='text-pink'>#</span>Projects</h2>
+            <h2 className='text-white text-2xl'><span className='text-pink'>#</span>Skills</h2>
             <Image className='w-[50%]' src={Line} alt='Line' />
         </div>
         <div className='flex flex-col gap-6 mt-[34px]'>
-            { projects.map((item, idx)=>(  
+            { skills.map((item, idx)=>(
                 <div key={idx} className='flex flex-col gap-2 pb-2 border-[1px] border-gray text-gray'>
-                    <div>
-                        <Image className='w-full h-48' priority src={item.image} alt='crud' height={192} width={192} />
+                    <div className='pl-3 py-2 border-b-[1px] border-gray'>
+                        <h2 className='text-2xl text-white'>{item.title}</h2>
                     </div>
-                    <div className='pl-1 border-b-[1px] border-gray'>
-                        <p className='text-base'>{item.technologies}</p>
-                    </div>
-                    <div className='pl-3 flex flex-col gap-4 pb-2'>
-                        <h2 className='text-2xl text-white'>{item.name}</h2>
-                        <p>{item.description}</p>
-                        <div className='flex gap-2'>
-                            <a className='p-1 text-center border-[1px] border-gray' href={item.demo}>Demo</a>
-                            <a className='p-1 text-center border-[1px] border-gray' href={item.code}>Code</a>
+                    { item.content.map((item, idx)=>(
+                        <div key={idx} className='pl-3 flex flex-col gap-4'>
+                            <p className='text-xl text-gray'>{item.name}</p>
                         </div>
-                    </div>
+                    ))}
                 </div>
             ))}
         </div>
-   
-        <div></div>
     </section>
   )
 }
