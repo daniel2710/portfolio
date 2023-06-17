f43daad2c1d0860076d1bf765fc4af121e2b618f
daniel2710 <daniel2710es@gmail.com>
2023-06-16 23:46:46 -0500

    work experience

---
+++ data/work.ts
@@ -0,0 +1,14 @@
+export const works = [
+    {
+        logo: '/../public/img/work/peacedev.png',
+        position: 'Frontend Developer',
+        name: 'PEACEDEV TEAM SAS',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio nihil minus molestiae doloremque eum deserunt deleniti magni pariatur est eaque, temporibus nobis tempora dicta similique adipisci sequi non. Porro, perferendis.',
+    },
+    {
+        logo: '/../public/img/work/peacedev.png',
+        position: 'Frontend Developer',
+        name: 'PEACEDEV TEAM SAS',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio nihil minus molestiae doloremque eum deserunt deleniti magni pariatur est eaque, temporibus nobis tempora dicta similique adipisci sequi non. Porro, perferendis.',
+    },
+]
