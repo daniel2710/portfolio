1e1b813fed15986581e9d1bf8194fccbe88dbabd
daniel2710 <daniel2710es@gmail.com>
2023-06-16 20:41:37 -0500

    projects section

---
+++ data/projects.ts
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+        image: '/../public/img/projects/crud.png',
+        technologies: 'Css Html Javascript',
+        name: 'Project 1',
+        description: 'A frontend project',
+        code: 'https://github.com/daniel2710',
+        demo: 'https://github.com/daniel2710'
+    },
+    {
+        image: '/../public/img/projects/crud.png',
+        technologies: 'Css Html Javascript',
+        name: 'Project 1',
+        description: 'A frontend project',
+        code: 'https://github.com/daniel2710',
+        demo: 'https://github.com/daniel2710'
+    },
+    {
+        image: '/../public/img/projects/crud.png',
+        technologies: 'Css Html Javascript',
+        name: 'Project 1',
+        description: 'A frontend project',
+        code: 'https://github.com/daniel2710',
+        demo: 'https://github.com/daniel2710'
+    },
+]
